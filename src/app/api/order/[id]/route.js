97fa35aba6f2c641b39d4c8cfcdcde16db5f6215
alzC <69784004+alzC@users.noreply.github.com>
2024-12-23 21:12:33 +0100

    update

--- src/app/api/order/[id]/route.js
+++ src/app/api/order/[id]/route.js
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import clientPromise from '@/lib/mongodb';
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET({ params }) {
     try {
         const client = await clientPromise;
         const db = client.db('restaurant');
